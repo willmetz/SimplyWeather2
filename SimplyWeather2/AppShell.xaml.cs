c4a08b386948ff4bb4d7115b7645a310cc9fccc4
wmetz <wmetz@ostusa.com>
2020-10-20 21:15:49 -0400

    Initial commit

---
+++ SimplyWeather2/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using SimplyWeather2.ViewModels;
+using SimplyWeather2.Views;
+using Xamarin.Forms;
+
+namespace SimplyWeather2
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
