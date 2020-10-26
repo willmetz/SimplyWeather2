b74eb9adf22d5748a060be928945a4df2843fece
wmetz <wmetz@ostusa.com>
2020-10-25 22:20:39 -0400

    Setup initial home page with bindable data now.  Next step is to wire it in with real data.

--- SimplyWeather2/Home/HomeViewModel.cs
+++ SimplyWeather2/Home/HomeViewModel.cs
@@ -1,10 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using SimplyWeather2.ViewModels;
+
 namespace SimplyWeather2.Home
 {
-    public class HomeViewModel
+    public class HomeViewModel : BaseViewModel
     {
+
+        private string _locationName;
+        public string LocationName
+        {
+            get => _locationName;
+            set {
+                _locationName = value;
+                RaiseAndSetIfChanged(nameof(LocationName));
+            }
+        }
+
+        private List<HourlyForecastItem> _hourlyForecastItems;
+        public List<HourlyForecastItem> HourlyForecastItems
+        {
+            get => _hourlyForecastItems;
+            set {
+                _hourlyForecastItems = value;
+                RaiseAndSetIfChanged(nameof(HourlyForecastItems));
+            }
+        }
+
         public HomeViewModel()
         {
+            HourlyForecastItems = new List<HourlyForecastItem>
+            {
+                new HourlyForecastItem{Temperature = "55" },
+                new HourlyForecastItem{Temperature = "56" },
+                new HourlyForecastItem{Temperature = "57" },
+                new HourlyForecastItem{Temperature = "58" }
+            };
+
+            LocationName = "Grand Rapids";
         }
     }
 }
---
+++ SimplyWeather2/Home/HourlyForecastItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace SimplyWeather2.Home
+{
+    public class HourlyForecastItem
+    {
+        private string _temperature;
+        public string Temperature {
+            get
+            {
+                return $"{_temperature} degrees";
+            }
+            set
+            {
+                _temperature = value;
+            }
+        }
+
+        private string _timeOfDay;
+        public string TimeOfDay
+        {
+            get => "10pm";   
+        }
+
+        private string _imageUrl;
+        public string ImageUrl {
+            get => "image";
+        }
+
+
+    }
+}
