5804b7ea731e300da73d30bcdb7b41552d27a20a
wmetz <wmetz@ostusa.com>
2020-11-17 15:33:40 -0500

    Added zoom level to app preferences

--- SimplyWeather2/Services/AppPreferencesService.cs
+++ SimplyWeather2/Services/AppPreferencesService.cs
@@ -11,30 +11,33 @@ namespace SimplyWeather2.Services
         void SetLocationName(string locationName);
         SimplyWeatherLocation GetLocation();
         string GetLocationName();
+        int GetZoomLevel();
+        void SetZoomLevel(int zoom);
     }
 
     public class AppPreferencesServiceImp : AppPreferencesService
     {
-        private readonly string LatitudeKey = "Latitude";
-        private readonly string LongitudeKey = "Longitude";
-        private readonly string LocationNameKey = "LocationNameKey";
+        private readonly string _latitudeKey = "Latitude";
+        private readonly string _longitudeKey = "Longitude";
+        private readonly string _locationNameKey = "LocationNameKey";
+        private readonly string _zoomKey = "ZoomKey";
 
         public void SetLocationInfo(SimplyWeatherLocation location)
         {
-            Preferences.Set(LatitudeKey, location.Latitude);
-            Preferences.Set(LongitudeKey, location.Longitude);
+            Preferences.Set(_latitudeKey, location.Latitude);
+            Preferences.Set(_longitudeKey, location.Longitude);
         }
 
         public void SetLocationName(string locationName)
         {
-            Preferences.Set(LocationNameKey, locationName);
+            Preferences.Set(_locationNameKey, locationName);
         }
 
         public SimplyWeatherLocation GetLocation()
         {
             const double Unknown = -9999;
-            double longitude = Preferences.Get(LongitudeKey, Unknown);
-            double latitude = Preferences.Get(LatitudeKey, Unknown);
+            double longitude = Preferences.Get(_longitudeKey, Unknown);
+            double latitude = Preferences.Get(_latitudeKey, Unknown);
 
             if(latitude != Unknown && longitude != Unknown)
             {
@@ -46,7 +49,17 @@ namespace SimplyWeather2.Services
 
         public string GetLocationName()
         {
-            return Preferences.Get(LocationNameKey, string.Empty);
+            return Preferences.Get(_locationNameKey, string.Empty);
+        }
+
+        public int GetZoomLevel()
+        {
+            return Preferences.Get(_zoomKey, 5);
+        }
+
+        public void SetZoomLevel(int zoom)
+        {
+            Preferences.Set(_zoomKey, zoom);
         }
     }
 }
